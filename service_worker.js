2e14e5d4c3cc6e2af4a003312aa496d7fa11f204
luckybeast <remu.teshigawara@gmail.com>
2019-11-06 07:25:18 +0000

    auto-built @24e4ba2310cda3491c9d74b27eea421d3af4f3e5

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2019-11-06T06:42:57.773Z */
+/* workbox 2019-11-06T07:22:53.863Z */
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
@@ -16,7 +16,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline/",
-        revision: "1573022577773",
+        revision: "1573024973863",
     }
 ]);
 
